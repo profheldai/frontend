0fc71bea8c998c41ed816e7afcbe2dc6df7fc8d2
heldai <heldai@localhost.localdomain>
2024-04-09 23:01:43 -0300

    fontend example

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function fut() {
+    $ball = document.getElementById("ball");
+    $ball.src="https://cdnv2.moovin.com.br/deckeronline/imagens/produtos/original/bola-futebol-campo-topper-slick-22-impermeavel-8f329acbe17ef77842b07aed4624d6d5.jpg";
+}
+
+function bask() {
+    $ball = document.getElementById("ball");
+    $ball.src="https://images.tcdn.com.br/img/img_prod/354638/bola_basquete_sports_oficial_mirim_tamanho_5_12271_variacao_2783_1_7550fd1638fa91fc9cdb22b07d89e1a9.jpg";
+}
+
+function voley() {
+    $ball = document.getElementById("ball");
+    $ball.src="https://lojaglobosports.com.br/image/cache/catalog/t7F8imfM2OZW7y1unzKmeeYz8uTI2izX1amnwbaBBoXsaLwNBQfftSDtoZLsnDYz5GjKMZ-1200x1200.jpg";
+}
+
+function tenis() {
+    $ball = document.getElementById("ball");
+    $ball.src="https://cdn.pixabay.com/photo/2022/04/17/11/41/tennis-7137976_640.jpg";
+}
